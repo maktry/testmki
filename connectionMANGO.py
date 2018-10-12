7c8739c51790e686827f540ce4dfc0f24c35f0fc
maktry <maktry@gmail.com>
2018-10-12 16:14:55 +1100

    Create connectionMANGO.py

---
+++ connectionMANGO.py
@@ -0,0 +1,45 @@
+from pymongo import MongoClient
+import json
+
+conn = MongoClient()
+db = conn["tumblelog"]
+
+# conn.list_database_names()
+# for x in range(1,10):
+#     print(str(db[x]))
+# collection = db.myclassb
+# //////////////// TO PRINT DB NAMES
+
+
+
+xx =db.list_collections(include_system_collections=False)
+
+for collect in xx:
+    print (collect)
+# /*/////////////////// TO PRINT COLLECTION - TABLES
+#  WE HAVE HAVE TWO --USER AND --TYPE HERE.
+
+
+collection =db.get_collection("post")
+
+cursor= collection.find()
+print(cursor.count())
+print(type(cursor[1]))
+
+for m in cursor[2]:
+    print(m)
+# #
+for rec in cursor:
+    print((rec["author"] ,"   :   ", rec["title"]))
+# ////////////////// the above code to print somthing from the COLLECTION
+
+collection.insert_one({"author":"jamal", "title":"awesome"})
+
+# ////////////////////// insert into mongodb
+
+
+the_one = collection.find({"author":"jamal"})
+print("----------------------------- %s", type(the_one))
+print(the_one[0])
+
+# /////// find from mangodB
